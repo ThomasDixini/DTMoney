7881129b2263b8fcf70b8530ba5dcdbf5fe551ed
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2023-04-23 22:53:03 -0300

    Summary(feat): Parte visual do resumo da aplicação criado

--- src/pages/Transactions/Transactions.tsx
+++ src/pages/Transactions/Transactions.tsx
@@ -1,9 +1,11 @@
 import { Header } from "../../components/Header/Header";
+import { Summary } from "../../components/Summary/index";
 
 export function Transactions() {
     return (
         <>
             <Header />
+            <Summary />
         </>
     );
 }
