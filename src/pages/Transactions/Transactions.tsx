72fa7322417c653bd7a343007439dea17d993717
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2023-04-23 23:19:00 -0300

    TransactionsPage(feat): Visual e HTML da Tabela de transações criada

--- src/pages/Transactions/Transactions.tsx
+++ src/pages/Transactions/Transactions.tsx
@@ -1,11 +1,39 @@
 import { Header } from "../../components/Header/Header";
 import { Summary } from "../../components/Summary/index";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
     return (
         <>
             <Header />
             <Summary />
+
+            <TransactionsContainer>
+                <TransactionsTable>
+                        <tbody>
+                            <tr>
+                                <td width="50%"> Desenvolvimento de site </td>
+                                <td> 
+                                    <PriceHighlight variant="income">
+                                        R$ 12.000,00 
+                                    </PriceHighlight>
+                                </td>
+                                <td> Venda </td>
+                                <td> 10/04/23 </td>
+                            </tr>
+                            <tr>
+                                <td width="50%"> Desenvolvimento de site </td>
+                                <td>
+                                    <PriceHighlight variant="outcome">
+                                        -R$ 12.000,00 
+                                    </PriceHighlight>
+                                </td>
+                                <td> Venda </td>
+                                <td> 10/04/23 </td>
+                            </tr>
+                        </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </>
     );
 }
