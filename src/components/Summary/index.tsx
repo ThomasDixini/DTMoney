48f5c934d777009ee89e15e3e483530bb60391e8
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2023-04-30 23:50:39 -0300

    hooks(refactor): Extraindo lógica do calculo do resumo de transações para um hook

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,27 +1,10 @@
 import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from "phosphor-react";
 import { SummaryCard, SummaryContainer } from "./styles";
-import { TransactionsContext } from "../../context/TransactionsContext";
-import { useContext } from 'react'
 import { priceFormatter } from "../../utils/formatter";
+import { useSummary } from "../../hooks/useSummary";
  
 export function Summary() {
-    const { transacions } = useContext(TransactionsContext)
-
-    const summary = transacions.reduce((acc, transaction) => {
-        if(transaction.type == 'income'){
-            acc.income += transaction.price;
-            acc.total += transaction.price;
-        } else if(transaction.type == 'outcome'){
-            acc.outcome += transaction.price;
-            acc.total -= transaction.price;
-        }
-
-        return acc
-    },{
-        income: 0,
-        outcome: 0,
-        total: 0
-    })
+    const summary = useSummary();
 
     return (
         <SummaryContainer>
