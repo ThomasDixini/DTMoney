56df17db4b24e1e2edf003dd0c5c9fb6b74ae377
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2023-04-30 23:45:28 -0300

    formatter(feat): Criando formatação dos dados ps:(Datas e preços)

--- src/pages/Transactions/Transactions.tsx
+++ src/pages/Transactions/Transactions.tsx
@@ -4,6 +4,7 @@ import { SearchForm } from "../../components/SearchForm";
 import { Summary } from "../../components/Summary/index";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 import { TransactionsContext } from "../../context/TransactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 
 export interface Transaction {
     id: number,
@@ -33,11 +34,12 @@ export function Transactions() {
                                             <td width="50%"> {transaction.description} </td>
                                             <td> 
                                                 <PriceHighlight variant={transaction.type}>
-                                                    {transaction.price} 
+                                                    {transaction.type == 'outcome' && '- '}
+                                                    {priceFormatter.format(transaction.price)} 
                                                 </PriceHighlight>
                                             </td>
                                             <td> {transaction.category} </td>
-                                            <td> {transaction.createdAt} </td>
+                                            <td> {dateFormatter.format(new Date(transaction.createdAt))} </td>
                                         </tr>
                                     );
                                 })
